06aaaa4a5cac39c7a550bcada32e3cce4ee980cf
zigzzie <adityagns14@gmail.com>
2021-11-15 18:54:48 +0530

    added date picker

--- src/components/APIcalls/SelectedCurrency.js
+++ src/components/APIcalls/SelectedCurrency.js
@@ -3,9 +3,18 @@ import GetCurrencyData from "./GetCurrencyData";
 function SelectedCurrency(props) {
   const url =
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
+  const url2 = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/";
+
   return (
     <GetCurrencyData first={props.firstCurrency} second={props.secondCurrency}>
-      {url + props.firstCurrency + "/" + props.secondCurrency + ".json"}
+      {url2 +
+        props.date +
+        "/" +
+        "currencies/" +
+        props.firstCurrency +
+        "/" +
+        props.secondCurrency +
+        ".json"}
     </GetCurrencyData>
   );
 }
